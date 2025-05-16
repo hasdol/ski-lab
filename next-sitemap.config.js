838a72dce2b0118420188b76f61cb899fd376d82
hasdol <hasdol98@gmail.com>
2025-05-16 11:16:45 +0200

    SEO improvemt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
-/** next-sitemap.config.js */
+// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://ski-lab.com',
   generateRobotsTxt: true,
-  sitemapSize: 7000,
+
+  // no splitting or manual additions
   changefreq: 'daily',
   priority: 0.7,
 
-  // ONLY your protected/auth-only routes go here.
-  // We use ** to recurse into all child paths.
+  // your protected routes
   exclude: [
     '/account',
     '/account/**',
@@ -19,15 +19,15 @@ module.exports = {
     '/skis/add/**',
     '/skis/locked',
     '/skis/locked/**',
-    '/skis/**/edit',
-    '/skis/**/edit/**',
-    '/results/**/edit',
+    '/results/*/edit',
     '/testing',
     '/testing/**',
     '/create-event',
     '/create-event/**',
     '/edit',
     '/edit/**',
+    '/teams/create-team',
+    '/teams/create-team/**',
   ],
 
   robotsTxtOptions: {
@@ -40,17 +40,14 @@ module.exports = {
           '/plans',
           '/skis/add',
           '/skis/locked',
-          '/skis/*/edit',
           '/results/*/edit',
           '/testing',
           '/create-event',
           '/edit',
         ],
-        allow: ['/'],            // everything else is crawlable
+        allow: ['/'],
       },
     ],
-    additionalSitemaps: [
-      'https://ski-lab.com/sitemap-0.xml',
-    ],
+    // no additionalSitemaps
   },
-}
+};
